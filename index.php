d0feff216b220911a50dc9419937f8636882082b
Alessio <alexguada@hotmail.it>
2018-12-11 09:05:17 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php include "assets/html/head.html"; ?>
+
+    <title>Home | Quit108</title>
+
+  </head>
+  <body>
+
+    <?php include "assets/html/nav.html"; ?>
+
+    <div class="container">
+	<center class="main">
+	<img src="img/banner/quit108.jpg" width="100%">
+	</center>
+	   <center class="live">
+   	     <h1>Live Streaming</h1>
+	     <iframe style="display: inline-block; margin-top: 30px; margin-bottom: 30px;" src="https://player.twitch.tv/?channel=quit108"" frameborder="0" allowfullscreen></iframe>
+	     <img src="img/banner/schedule.png" width="50%">
+	   </center>
+
+	  </div>
+    </div>
+
+<?php include "assets/html/footer.html"; ?>
+ 
---
+++ teamspeak.php
@@ -0,0 +1,34 @@
+<?php include "assets/html/head.html"; ?>
+
+    <title>TeamSpeak | Quit108</title>
+
+  </head>
+  <body>
+
+    <?php include "assets/html/nav.html"; ?>
+
+    <div class="container">
+      <div class="text long_text" id="home_text">
+	   <center><h1>TeamSpeak 108Community</h1></center>
+           <center><img src="img/banner/teamspeak.jpg" width="100%"></center>
+           <p>Il Server TeamSpeak nasce dalla collaborazione tra Quit108 e <a href="http://url.quit108.it/ty.html">TrinityHosting</a> azienda molto all'avanguarda nel settore hosting in italia</p>
+
+           <center style="margin-top: 30px; margin-bottom: 30px;"><h1>Regolamento</h1></center>
+
+     <ol>
+       <li>&Egrave; SEVERAMENTE VIETATO REGISTRARE nelle stanze pubbliche e private.</li>
+       <li>Vietato utilizzare nickname provocatori, offensivi e con bestemmie. </li>
+       <li>Vietato utilizzare i nickname di membri dello STAFF.</li>
+       <li>Vietato spammare link o quant' altro.</li>
+       <li>Vietato bestemmiare e insultare.</li>
+       <li>Vietato utlizzare plugins come soundboard o bot musicali in stanze pubbliche.</li>
+       <li>Obbligatorio il push to talk nelle stanze pubbliche.</li>
+       <li>Vietato dare fastidio ad altri utenti (es: molestare, urlare). </li>
+       <li>Vietato utilizzare avatar personali con immagini offensive (es: nudit&agrave;,porno,razzismo ecc..)</li>
+       <li>Vietate discussioni, utilizzo di immagini e descrizioni (in stanze pubbliche o private senza password) su Religione, Politica, Calcio e Sessualit&agrave;.</li>
+       <li>LO STAFF NON SI PRENDE ALCUNA RESPONSABILIT&Agrave; PER I LINK E FILE CONDIVISI NELLA STANZE PRIVATE.</li>
+      </lo>
+       </div>
+    </div>
+
+<?php include "assets/html/footer.html"; ?>
